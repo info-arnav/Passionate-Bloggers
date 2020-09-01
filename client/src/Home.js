eea18b0491243caddaeb4b68492fb205b47c60bd
Arnav <info@arnavgupta.net>
2020-09-02 00:30:36 +0530

    home loading error now corrected

--- client/src/Home.js
+++ client/src/Home.js
@@ -46,51 +46,6 @@ const Home = () => {
         </div>
       </nav>
       <main class="page lanidng-page">
-        {```<section class="portfolio-block block-intro">
-          <div class="container">
-            <div class="avatar" id="img1">
-              {
-                //style='background-image:url("assets/img/avatars/avatar.jpg");'
-              }
-            </div>
-            <div class="about-me">
-              <p>Arnav Gupta</p>
-            </div>
-          </div>
-        </section>
-        <section class="portfolio-block photography">
-          <div class="container">
-            <div class="row no-gutters">
-              <div class="col-md-6 col-lg-4 item zoom-on-hover">
-                <a href="/projects">
-                  <img
-                    alt=""
-                    class="img-fluid image"
-                    src="assets/img/nature/image10.jpg"
-                  />
-                </a>
-              </div>
-              <div class="col-md-6 col-lg-4 item zoom-on-hover">
-                <a href="/projects">
-                  <img
-                    alt=""
-                    class="img-fluid image"
-                    src="assets/img/nature/image2.jpeg"
-                  />
-                </a>
-              </div>
-              <div class="col-md-6 col-lg-4 item zoom-on-hover">
-                <a href="/projects">
-                  <img
-                    alt=""
-                    class="img-fluid image"
-                    src="assets/img/nature/image11.jpg"
-                  />
-                </a>
-              </div>
-            </div>
-          </div>
-        </section>```}
         <section class="portfolio-block call-to-action border-bottom">
           <div class="container">
             <div class="d-flex justify-content-center align-items-center content">
