7b16f879337204608a4e2fe096d842ea1db6a607
Arnav <info@arnavgupta.net>
2020-09-03 20:10:11 +0530

    single blog react-router to react-router-dom

--- client/src/main/Single.js
+++ client/src/main/Single.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Navigation from "../elements/Navigation";
-import { useParams } from "react-router";
+import { useParams } from "react-router-dom";
 
 const Single = () => {
   let { id } = useParams();
