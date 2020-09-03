ca242964c7316eb4e4349dc9ce3463a520b3ebe8
Arnav <info@arnavgupta.net>
2020-09-04 01:08:47 +0530

    test part 2 now done

--- client/src/Single.js
+++ client/src/Single.js
@@ -4,19 +4,13 @@ import { useParams } from "react-router-dom";
 
 const Single = () => {
   let { id } = useParams();
-  const [post, updater] = useState({});
-  useEffect(() =>
-    fetch(`/single/post/${id}`)
-      .then((e) => e.json())
-      .then((e) => updater(e))
-  );
   return (
     <div>
       <Navigation />
       <main class="page blog-post">
         <section class="clean-block clean-post dark">
           <div class="container">
-            <div class="block-content" />
+            <div class="block-content">{id}</div>
           </div>
         </section>
       </main>
