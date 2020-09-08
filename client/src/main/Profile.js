370cd170f7d86bef0b592c88e7f54437a3c439b6
Arnav <info@arnavgupta.net>
2020-09-08 11:46:08 +0530

    className from class for better code ig

--- client/src/main/Profile.js
+++ client/src/main/Profile.js
@@ -18,33 +18,33 @@ const Profile = () => {
   return (
     <div>
       <Navigation />
-      <main class="page">
-        <section class="clean-block about-us">
-          <div class="container">
-            <div class="block-heading">
-              <h2 class="text-info">About</h2>
+      <main className="page">
+        <section className="clean-block about-us">
+          <div className="container">
+            <div className="block-heading">
+              <h2 className="text-info">About</h2>
             </div>
-            <div class="row justify-content-center">
-              <div class="col-sm-6 col-lg-4">
-                <div class="card clean-card text-center">
+            <div className="row justify-content-center">
+              <div className="col-sm-6 col-lg-4">
+                <div className="card clean-card text-center">
                   <img
-                    class="card-img-top w-100 d-block"
+                    className="card-img-top w-100 d-block"
                     src="assets/img/avatars/avatar1.jpg"
                   />
-                  <div class="card-body info">
-                    <h4 class="card-title">{data.name}</h4>
-                    <p class="card-text">{data.biology}</p>
+                  <div className="card-body info">
+                    <h4 className="card-title">{data.name}</h4>
+                    <p className="card-text">{data.biology}</p>
                     <p>followers - {data.followers.length}</p>
                     <p>followers - {data.following.length}</p>
-                    <div class="icons">
+                    <div className="icons">
                       <a href="#">
-                        <i class="icon-social-facebook" />
+                        <i className="icon-social-facebook" />
                       </a>
                       <a href="#">
-                        <i class="icon-social-instagram" />
+                        <i className="icon-social-instagram" />
                       </a>
                       <a href="#">
-                        <i class="icon-social-twitter" />
+                        <i className="icon-social-twitter" />
                       </a>
                     </div>
                   </div>
