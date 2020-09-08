68a131d0076d4979307a35b0965673c7d5f11c12
Arnav <info@arnavgupta.net>
2020-09-08 12:19:49 +0530

    Active

--- client/src/main/Active.js
+++ client/src/main/Active.js
@@ -3,12 +3,14 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../actions/authActions";
 import { Link } from "react-router-dom";
+import Navigation from "../elements/Navigation";
 
 class Active extends Component {
   constructor() {
     super();
     this.state = {
       status: [],
+      datass: { followers: [], following: [] },
       email: "",
       password: "",
       errors: {},
@@ -19,12 +21,6 @@ class Active extends Component {
     e.preventDefault();
     this.props.logoutUser();
   };
-
-  async componentDidMount() {
-    await fetch("/user/auth/status")
-      .then((e) => e.text())
-      .then((e) => this.setState({ status: e }));
-  }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.errors) {
@@ -46,11 +42,63 @@ class Active extends Component {
 
     this.props.loginUser(userData);
   };
+
+  async componentDidMount() {
+    const { user } = this.props.auth;
+    await fetch("/user/auth/status")
+      .then((e) => e.text())
+      .then((e) => this.setState({ status: e }));
+    await fetch(`/user/profile/data/${user.name}`)
+      .then((e) => e.json())
+      .then((e) => this.setState({ datass: e }));
+  }
+
   render() {
     const { data } = this.state;
+    const { datass } = this.state;
     const { user } = this.props.auth;
     const status = this.state.status;
-    return <div>{user.name}</div>;
+    return (
+      <div>
+        <Navigation />
+        <main className="page">
+          <section className="clean-block about-us">
+            <div className="container">
+              <div className="block-heading">
+                <h2 className="text-info">About</h2>
+              </div>
+              <div className="row justify-content-center">
+                <div className="col-sm-6 col-lg-4">
+                  <div className="card clean-card text-center">
+                    <img
+                      className="card-img-top w-100 d-block"
+                      src="assets/img/avatars/avatar1.jpg"
+                    />
+                    <div className="card-body info">
+                      <h4 className="card-title">{datass.name}</h4>
+                      <p className="card-text">{datass.biology}</p>
+                      <p>followers - {datass.followers.length}</p>
+                      <p>followers - {datass.following.length}</p>
+                      <div className="icons">
+                        <a href="#">
+                          <i className="icon-social-facebook" />
+                        </a>
+                        <a href="#">
+                          <i className="icon-social-instagram" />
+                        </a>
+                        <a href="#">
+                          <i className="icon-social-twitter" />
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </main>
+      </div>
+    );
   }
 }
 
